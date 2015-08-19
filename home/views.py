f6238b4ac9a9fb85001a3029a5087a06f0dc8fbe
Amaranthos <joshua.hodkinson.42@gmail.com>
2015-08-19 11:21:00 +1000

    Polished home page more, began work on games page

--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, "home/home.html", {})
+
+def games(request):
+	return render(request, "home/games.html", {})
--- jhcom/urls.py
+++ jhcom/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^$', 'home.views.home', name='home'),
+	url(r'^games/', 'home.views.games', name='games'),
     url(r'^admin/', include(admin.site.urls)),
 ]
