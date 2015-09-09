0957b25a9bb59241e159b93d40d24cd28db05e6f
Amaranthos <joshua.hodkinson.42@gmail.com>
2015-09-09 11:49:11 +1000

    Blog home page now displays date archive list, selecting a month, year displays a list of blogs from that month and year.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 urlpatterns = [
 	url(r'^$', 'blog.views.blog', name='blog'),
 	url(r'^category/(?P<id>[0-9]+)/(?P<slug>[-\w\d]+)/$', 'blog.views.category', name='blog-category'),
+	url(r'^date/(?P<year>[0-9]{4})/(?P<month>[0-9])/$', 'blog.views.dates', name='blog-date'),
 	url(r'^(?P<id>[0-9]+)/(?P<slug>[-\w\d]+)/$', 'blog.views.post', name='blog-post'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,31 @@
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404, render
+from datetime import date
 
 from .models import Blog, Category
+from calendar import month_name
 
 def blog(request):
 	return render(request, "blog/home.html", {
 			'categories' : Category.objects.all(),
+			'dates' : Blog.objects.all().values_list('posted', flat=True).order_by('posted'),
 			'posts' : Blog.objects.all().order_by('posted')[:5]
 		})
 
 def post(request, id, slug):
 	return render(request, "blog/post.html", {'post' : get_object_or_404(Blog, pk=id)})
 
+def dates(request, year, month):
+	collection = Blog.objects.filter(posted__year=year, posted__month=month)
+	return render(request, "blog/list.html", {
+			'title' : month_name[int(month)] + ", " + year,
+			'list' : collection,
+			'posts' : collection
+		})
+
 def category(request, id, slug):
 	category = get_object_or_404(Category, pk=id)
 	return render(request, "blog/list.html", {
+			'title' : category.title,
 			'list' : category,
 			'posts' : Blog.objects.filter(category=category)
 		})
